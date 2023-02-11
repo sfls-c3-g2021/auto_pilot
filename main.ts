37e127b41ca220b635380cd12a73e37ad2253e9a
sfls-c3-g2021 <108339377+sfls-c3-g2021@users.noreply.github.com>
2023-02-11 21:02:59 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+function will_crush (dis_value: number) {
+    return Tinybit.Ultrasonic_Car() <= dis_value
+}
+function set_color (color: number) {
+    Tinybit.RGB_Car_Big(color);
+}
+function get_line(color: Tinybit.enLineState, pos: Tinybit.enPos)
+{
+    return Tinybit.Line_Sensor(pos, color)
+}
 basic.forever(function () {
 	
 })
