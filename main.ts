123cbbf59855482b042b678d7aa99f69de946d2c
sfls-c3-g2021 <108339377+sfls-c3-g2021@users.noreply.github.com>
2023-02-11 21:08:48 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,5 +9,15 @@ function get_line(color: Tinybit.enLineState, pos: Tinybit.enPos)
     return Tinybit.Line_Sensor(pos, color)
 }
 basic.forever(function () {
-	
+    if (will_crush(45)) {
+        set_color(1)
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 60)
+        basic.pause(300)
+        Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_Run, 80, 0)
+        basic.pause(100)
+    } else {
+        set_color(2)
+        Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_Run, 80, 80)
+    }
 })
